4ff20067fddd0710757723cfe70729f40304c248
CERIO18 <ceurioste@derp.org>
2019-03-31 21:27:26 -0600

    working

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+
+
+
+CREATE TABLE burger
+(
+	id INT (10)AUTO_INCREMENT NOT NULL,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN,
+	PRIMARY KEY (id)
+);  
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE DATABASE:v5n9b6909rt5aqrk;
+CREATE TABLE burger
+(
+	id INT (10)AUTO_INCREMENT NOT NULL,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN,
+	PRIMARY KEY (id)
+);  
